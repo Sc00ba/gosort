07db7a4a6a4624b004c1b15e944fca191b8b9141
Scuba <sc00ba@letmevibecodethat.com>
2025-08-02 12:51:30 -0500

    improve shutdown in main

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"sync"
 
 	"golang.org/x/sys/unix"
 )
@@ -31,12 +32,14 @@ func main() {
 	if *memprofile != "" {
 		f, err := os.Create(*memprofile)
 		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
+			log.Printf("could not create memory profile (%s)", err)
+			return
 		}
 		defer f.Close()
 		runtime.GC()
 		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
+			log.Printf("could not write memory profile (%s)", err)
+			return
 		}
 		log.Printf("✅ Memory profile written to %s\n", *memprofile)
 	}
@@ -57,7 +60,8 @@ func runSort(outFileArg *string, bufferSizeArg *uint, parallel *uint) {
 	if *outFileArg != "" {
 		file, err := os.OpenFile(*outFileArg, os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
-			log.Fatalf("failed to open out file %s (%s)\n", *outFileArg, err)
+			log.Printf("Failed to open out file %s (%s)\n", *outFileArg, err)
+			return
 		}
 
 		outFile = file
@@ -67,12 +71,14 @@ func runSort(outFileArg *string, bufferSizeArg *uint, parallel *uint) {
 	var info unix.Sysinfo_t
 	err := unix.Sysinfo(&info)
 	if err != nil {
-		log.Fatalf("Error calling sysinfo: %s", err)
+		log.Printf("Error calling sysinfo: %s", err)
+		return
 	}
 
 	totalMemory := uint(float64(info.Totalram * uint64(info.Unit)))
 	if *bufferSizeArg > totalMemory {
-		log.Fatalf("-S argument %d is too large\n", *bufferSizeArg)
+		log.Printf("-S argument %d is too large\n", *bufferSizeArg)
+		return
 	}
 
 	bufferSize := uint(float64(info.Totalram*uint64(info.Unit)) * defaultMemoryPercent)
@@ -93,45 +99,65 @@ func runSort(outFileArg *string, bufferSizeArg *uint, parallel *uint) {
 	if readFromStdIn {
 		readers = append(readers, os.Stdin)
 	} else {
-		log.Fatal("stdin is the only input file currently supported")
+		log.Printf("Stdin is the only input file currently supported")
+		return
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var wg sync.WaitGroup
+
 	chunksChan, chunkerErrs, err := chunks.NewChunker(ctx, chunkSize, channelBufferSize, readers...)
 	if err != nil {
-		log.Fatalf("error while creating chunker (%s)", err)
+		cancel()
+		log.Printf("Error while creating chunker (%s)", err)
+		return
 	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range chunkerErrs {
+			log.Printf("Error from chunker (%s)", err)
+			cancel()
+		}
+	}()
 
 	sortedChunksChan, sortErrs, err := chunks.NewSorter(ctx, int(numSorters), chunksChan)
 	if err != nil {
-		log.Fatalf("error while creating sorter (%s)", err)
+		cancel()
+		log.Printf("Error while creating sorter (%s)", err)
+		wg.Wait()
+		return
 	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range sortErrs {
+			log.Printf("Error from sorter (%s)", err)
+			cancel()
+		}
+	}()
 
 	mergeFactor := 100
 	mergeErrs, err := chunks.NewMerger(ctx, sortedChunksChan, outFile, mergeFactor)
 	if err != nil {
-		log.Fatalf("error while creating merger (%s)", err)
+		cancel()
+		log.Printf("Error while creating merger (%s)", err)
+		wg.Wait()
+		return
 	}
 
-	run := true
-	for run {
-		select {
-		case err, ok := <-chunkerErrs:
-			if ok {
-				log.Fatal(err)
-			}
-		case err, ok := <-sortErrs:
-			if ok {
-				log.Fatal(err)
-			}
-		case err, ok := <-mergeErrs:
-			if ok {
-				log.Fatal(err)
-			} else {
-				run = false
-			}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range mergeErrs {
+			log.Printf("Error from merger (%s)", err)
+			cancel()
 		}
-	}
+	}()
+
+	wg.Wait()
 }
